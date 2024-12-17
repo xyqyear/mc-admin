83085a198b0aa54cd304437876cc705092df9121
xyqyear <xyqyear@gmail.com>
2024-12-17 11:01:18 +0800

    feat: add logout button to header

--- frontend/components/layouts/AppHeader.vue
+++ frontend/components/layouts/AppHeader.vue
@@ -1,5 +1,30 @@
+<script setup lang="ts">
+import { useTokenStore } from "~/stores/useTokenStore";
+
+const { clearToken } = useTokenStore();
+
+const logout = () => {
+  clearToken();
+  navigateTo("/login");
+};
+</script>
+
 <template>
-  <div class="h-12 flex justify-center items-center">
-    <h2 class="text-lg">Header</h2>
+  <div class="h-10 flex items-center">
+    <div class="empty-bar flex-1"></div>
+    <div
+      class="logout-icon flex justify-center items-center mr-2 p-1 rounded-md cursor-pointer"
+      @click="logout"
+    >
+      <ElIcon class="flex items-center" :size="20">
+        <NuxtIcon name="material-symbols:logout" />
+      </ElIcon>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.logout-icon:hover {
+  background-color: var(--el-menu-hover-bg-color);
+}
+</style>
