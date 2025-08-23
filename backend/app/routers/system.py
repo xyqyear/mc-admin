285f606ee04f7a3486470b9349da9fcc3f006eb4
xyqyear <xyqyear@gmail.com>
2025-08-23 16:56:53 +0800

    refactor: optimize get_server_info function for improved readability and performance

--- backend/app/routers/system.py
+++ backend/app/routers/system.py
@@ -32,15 +32,21 @@ class ServerInfo(BaseModel):
     "/info", dependencies=[Depends(get_current_user)], response_model=ServerInfo
 )
 def get_server_info():
+    cpu_percent = get_cpu_percent()
+    cpu_load = get_cpu_load()
+    memory_info = get_memory_info()
+    server_disk_info = get_disk_info(settings.server_path)
+    backup_disk_info = get_disk_info(settings.backup_path)
+
     return ServerInfo(
-        cpuPercentage=get_cpu_percent(),
-        cpuLoad1Min=get_cpu_load().one_minute,
-        cpuLoad5Min=get_cpu_load().five_minutes,
-        cpuLoad15Min=get_cpu_load().fifteen_minutes,
-        ramUsedGB=get_memory_info().used / 1024**3,
-        ramTotalGB=get_memory_info().total / 1024**3,
-        diskUsedGB=get_disk_info(settings.server_path).used / 1024**3,
-        diskTotalGB=get_disk_info(settings.server_path).total / 1024**3,
-        backupUsedGB=get_disk_info(settings.backup_path).used / 1024**3,
-        backupTotalGB=get_disk_info(settings.backup_path).total / 1024**3,
+        cpuPercentage=cpu_percent,
+        cpuLoad1Min=cpu_load.one_minute,
+        cpuLoad5Min=cpu_load.five_minutes,
+        cpuLoad15Min=cpu_load.fifteen_minutes,
+        ramUsedGB=memory_info.used / 1024**3,
+        ramTotalGB=memory_info.total / 1024**3,
+        diskUsedGB=server_disk_info.used / 1024**3,
+        diskTotalGB=server_disk_info.total / 1024**3,
+        backupUsedGB=backup_disk_info.used / 1024**3,
+        backupTotalGB=backup_disk_info.total / 1024**3,
     )
