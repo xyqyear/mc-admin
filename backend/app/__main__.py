5c29efb8fdcbc13f6d2a2d9e63ad266f9c02ff8d
xyqyear <xyqyear@gmail.com>
2025-08-29 02:32:49 +0800

    fix: update uvicorn run command to use app reference directly

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -3,4 +3,4 @@ import uvicorn
 from .main import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=5678, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=5678, reload=False)
