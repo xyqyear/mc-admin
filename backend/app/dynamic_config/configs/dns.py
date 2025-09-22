2be281789b39705c91feb503e5e698e65416dd75
xyqyear <xyqyear@gmail.com>
2025-09-22 12:32:16 +0800

    refactor(dns): remove Natmap and DockerWatcher configurations from DNS manager

--- backend/app/dynamic_config/configs/dns.py
+++ backend/app/dynamic_config/configs/dns.py
@@ -20,27 +20,6 @@ class Huawei(BaseConfigSchema):
     region: Annotated[str, Field(description="DNS区域")] = "cn-south-1"
 
 
-class NatmapMonitorConfig(BaseConfigSchema):
-    enabled: Annotated[bool, Field(description="是否启用Natmap监控器")] = False
-    base_url: Annotated[str, Field(description="Natmap监控器的基础URL")] = (
-        "http://127.0.0.1:8080"
-    )
-
-
-class DockerWatcherConfig(BaseConfigSchema):
-    enabled: Annotated[bool, Field(description="是否启用Docker监控器")] = False
-    # use settings.server_path for servers_root_path
-    # servers_root_path: Annotated[str, Field(default="/path/to/servers", description="The root path for the Docker servers")]
-
-
-class Natmap(BaseConfigSchema):
-    type: Annotated[Literal["natmap"], Field(description="地址配置的类型")] = "natmap"
-    name: Annotated[str, Field(description="地址名称")] = "*"
-    internal_port: Annotated[int, Field(description="Minecraft服务器的内部端口")] = (
-        25565
-    )
-
-
 class Manual(BaseConfigSchema):
     type: Annotated[Literal["manual"], Field(description="地址配置的类型")] = "manual"
     name: Annotated[str, Field(description="地址名称")] = "*"
@@ -63,20 +42,9 @@ class DNSManagerConfig(BaseConfigSchema):
         "http://127.0.0.1:26666"
     )
 
-    natmap_monitor: Annotated[
-        NatmapMonitorConfig,
-        Field(description="Natmap监控器配置"),
-    ] = NatmapMonitorConfig()
-    docker_watcher: Annotated[
-        DockerWatcherConfig,
-        Field(description="Docker监控器配置"),
-    ] = DockerWatcherConfig()
-
     dns_ttl: Annotated[int, Field(description="DNS TTL（生存时间）值")] = 15
 
-    poll_interval: Annotated[int, Field(description="轮询间隔（秒）")] = 15
-
     addresses: Annotated[
-        list[Annotated[Union[Natmap, Manual], Field(discriminator="type")]],
+        list[Manual],
         Field(description="地址配置列表", default_factory=list),
     ]
